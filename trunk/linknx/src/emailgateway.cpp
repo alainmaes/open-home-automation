73b839b1340ae435aeaae1878b104872ae4c96c3
alain.maes007@gmail.com <alain.maes007@gmail.com@3cdfd51e-d06c-b4ec-2b34-f7991c7d37ae>
2012-10-05 22:11:32 +0000

    smtp support via curl

--- trunk/linknx/src/emailgateway.cpp
+++ trunk/linknx/src/emailgateway.cpp
@@ -22,6 +22,8 @@
 #ifdef HAVE_LIBESMTP
 #include <libesmtp.h>
 #include <signal.h>
+#elif defined HAVE_LIBCURL //OPEN_HOME_AUTOMATION
+#include <curl/curl.h>
 #endif
 
 Logger& EmailGateway::logger_m(Logger::getInstance("EmailGateway"));
@@ -37,13 +39,19 @@ void EmailGateway::importXml(ticpp::Element* pConfig)
     std::string type = pConfig->GetAttribute("type");
     if (type == "smtp")
     {
-#ifdef HAVE_LIBESMTP
+#ifndef HAVE_LIBCURL
+ std::stringstream msg_1;
+        msg_1 << "EmailGateway: libCuRL not available" << std::endl;
+        throw ticpp::Exception(msg_1.str());
+#endif
+
+#if (defined HAVE_LIBESMTP || defined HAVE_LIBCURL) //OPEN_HOME_AUTOMATION
         type_m = SMTP;
         pConfig->GetAttribute("host", &host_m);
         pConfig->GetAttribute("from", &from_m);
         login_m = pConfig->GetAttribute("login");
         pass_m = pConfig->GetAttribute("pass");
-# else
+#else
         std::stringstream msg;
         msg << "EmailGateway: Gateway type 'smtp' not supported, libesmtp not available" << std::endl;
         throw ticpp::Exception(msg.str());
@@ -149,6 +157,63 @@ const char *MessageBody::getData(int *len)
     return NULL;
 }
 
+#ifdef HAVE_LIBCURL //OPEN_HOME_AUTOMATION
+struct upload_status {
+  const char *from;
+  const char *to;
+  const char *subject;
+  const char *text;
+  int lines_read;
+};
+ 
+static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *userp)
+{
+    struct upload_status *upload_ctx = (struct upload_status *)userp;
+    size_t len;
+
+    if ((size == 0) || (nmemb == 0) || ((size*nmemb) < 1))
+    {
+        return 0;
+    }
+
+    if (upload_ctx->lines_read == 0)
+    {
+        len = sprintf((char *)ptr, "To: %s\n", upload_ctx->to);
+        upload_ctx->lines_read ++;
+        return len;
+    }
+    else if (upload_ctx->lines_read == 1)
+    {
+        len = sprintf((char *)ptr, "From: %s\n", upload_ctx->from);
+        upload_ctx->lines_read ++;
+        return len;
+    }
+    else if (upload_ctx->lines_read == 2)
+    {
+        len = sprintf((char *)ptr, "Subject: %s\n", upload_ctx->subject);
+        upload_ctx->lines_read ++;
+        return len;
+    }
+    else if (upload_ctx->lines_read == 3)
+    {
+        /* empty line to divide headers from body, see RFC5322 */
+        len = sprintf((char *)ptr, "\n", upload_ctx->from);
+        upload_ctx->lines_read ++;
+        return len;
+    }
+    else if (upload_ctx->lines_read == 4)
+    {
+        len = sprintf((char *)ptr, "%s\n", upload_ctx->text);
+        upload_ctx->lines_read ++;
+        return len;
+    }
+    else
+    {
+        return 0;
+    }
+}
+#endif
+
 void EmailGateway::sendEmail(std::string &to, std::string &subject, std::string &text)
 {
     if (type_m == SMTP)
@@ -229,6 +294,75 @@ void EmailGateway::sendEmail(std::string &to, std::string &subject, std::string 
         smtp_destroy_session (session);
         auth_destroy_context (authctx);
         auth_client_exit ();
+#elif defined HAVE_LIBCURL
+        //OPEN_HOME_AUTOMATION
+        CURL                 *curl;
+        CURLcode              res;
+        struct curl_slist    *recipients = NULL;
+        struct upload_status  upload_ctx;
+ 
+        upload_ctx.lines_read = 0;
+        upload_ctx.to = to.c_str();
+        upload_ctx.from = from_m.c_str();
+        upload_ctx.subject = subject.c_str();
+        upload_ctx.text = text.c_str();
+
+        curl = curl_easy_init();
+        if (curl)
+        {
+            //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+            /* this is the URL for your mailserver - you can also use an smtps:// URL
+             * here */ 
+            curl_easy_setopt(curl, CURLOPT_URL, host_m.c_str());
+            //TODO check for '/' in server name
+ 
+            /* Note that this option isn't strictly required, omitting it will result in
+             * libcurl will sent the MAIL FROM command with no sender data. All
+             * autoresponses should have an empty reverse-path, and should be directed
+             * to the address in the reverse-path which triggered them. Otherwise, they
+             * could cause an endless loop. See RFC 5321 Section 4.5.5 for more details.
+             */ 
+            curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from_m.c_str());
+ 
+            /* Note that the CURLOPT_MAIL_RCPT takes a list, not a char array.  */ 
+            recipients = curl_slist_append(recipients, to.c_str());
+            curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+ 
+            /* You provide the payload (headers and the body of the message) as the
+             * "data" element. There are two choices, either:
+             * - provide a callback function and specify the function name using the
+             * CURLOPT_READFUNCTION option; or
+             * - just provide a FILE pointer that can be used to read the data from.
+             * The easiest case is just to read from standard input, (which is available
+             * as a FILE pointer) as shown here.
+             */ 
+	    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+            curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+ 
+	    /* send the message (including headers) */ 
+	    res = curl_easy_perform(curl);
+            /* Check for errors */ 
+            if (res != CURLE_OK)
+                logger_m.errorStream() << "EmailGateway: CuRL SMTP failed: " << curl_easy_strerror(res) << endlog;
+ 
+            /* free the list of recipients */ 
+            curl_slist_free_all(recipients);
+ 
+            /* curl won't send the QUIT command until you call cleanup, so you should be
+             * able to re-use this connection for additional messages (setting
+             * CURLOPT_MAIL_FROM and CURLOPT_MAIL_RCPT as required, and calling
+             * curl_easy_perform() again. It may not be a good idea to keep the
+             * connection open for a very long time though (more than a few minutes may
+             * result in the server timing out the connection), and you do want to clean
+             * up in the end.
+             */ 
+             curl_easy_cleanup(curl);
+        }
+        else
+        {
+            logger_m.errorStream() << "EmailGateway: Failed to initialize CuRL" << endlog;
+        }
 #endif
     }
     else
